47bd86e2b9a00c1b1c3a8f9e64ecf1e1b161dd56
deveshaha <devesh_hanumante@outlook.com>
2022-05-12 11:07:54 +0200

    add Controller

---
+++ src/dam/control/RestaurantControl.java
@@ -0,0 +1,14 @@
+package dam.control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RestaurantControl implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
